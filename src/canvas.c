2f1b39bca08ea50f2cf901c910a8e51c64a7813d
brkp <brkp@tutanota.com>
2022-03-10 21:12:54 +0300

    implement canvas_free and bmp_free

--- src/canvas.c
+++ src/canvas.c
@@ -1,4 +1,5 @@
 #include "canvas.h"
+#include <stdint.h>
 
 BMPImage bmp_new(uint32_t w, uint32_t h) {
     BMPImage bmp = { w, h };
@@ -7,9 +8,23 @@ BMPImage bmp_new(uint32_t w, uint32_t h) {
     return bmp;
 }
 
+void bmp_free(BMPImage *image) {
+    free(image->pixel_data);
+
+    image->w = 0;
+    image->h = 0;
+    image->pixel_data = NULL;
+}
+
 Canvas canvas_new(uint32_t w, uint32_t h) {
     BMPImage bmp = bmp_new(w, h);
     Canvas c = { w, h, bmp };
 
     return c;
+}
+
+void canvas_free(Canvas *canvas) {
+    canvas->w = 0;
+    canvas->h = 0;
+    bmp_free(&canvas->image);
 }
