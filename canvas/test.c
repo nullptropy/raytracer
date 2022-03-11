0c8ccf9eab0f391b22407439746a8b2a20c64639
Ny <ny@ny.local>
2022-03-12 04:25:46 +1100

    Buggy implimentation of canvas_set_pixel and bmp_set_pixel or just the test program is buggy

--- canvas/test.c
+++ canvas/test.c
@@ -12,18 +12,23 @@ void debug_canvas(Canvas c) {
     printf("  pixel_data:\n");
 
     // TODO: broken rn since we're using an array of `Colors`
-    /* printf("   "); */
-    /* for (int x = 0; x < c.w; x++) { */
-    /*     for (int y = 0; y < c.h; y++) */
-    /*         printf("%d", c.image.pixel_data[y*c.w+x].b); */
-    /*     printf("\n   "); */
-    /* } */
-    /* printf("\b\b\b"); */
+    printf("   ");
+    for (int x = 0; x < c.w; x++) {
+        for (int y = 0; y < c.h; y++) {
+            printf("(%d,", c.image.pixel_data[c.w*y+x].r);
+            printf("%d,", c.image.pixel_data[c.w*y+x].g);
+            printf("%d) ", c.image.pixel_data[c.w*y+x].b);
+        }
+        printf("\n   ");
+    }
+    printf("\b\b\b");
 }
 
 
 int main() {
-    Canvas c = canvas_new(4, 4);
+    Canvas c = canvas_new(10, 10);
+    Color color = { 0, 255, 0 };
+    canvas_set_pixel(&c, 0, 5, color);
     debug_canvas(c);
         
     canvas_free(&c);
