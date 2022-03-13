671b1ef411faaa438ef3fbff924138d7df0e4270
brkp <brkp@tutanota.com>
2022-03-13 23:35:35 +0300

    implement export functionality

--- canvas/test.c
+++ canvas/test.c
@@ -3,31 +3,38 @@
 
 #include "canvas.h"
 
-void debug_canvas(Canvas c) {
-    printf("Canvas:\n");
-    printf("  w: %d\n", c.w);
-    printf("  h: %d\n", c.h);
-    printf("  BMPImage:\n");
-    printf("    w: %d\n", c.image.w);
-    printf("    h: %d\n", c.image.h);
-    printf("    pixel_data:");
-
-    for (int y = 0; y < c.h; y++) {
-        printf("\n     ");
-        for (int x = 0; x < c.w; x++) {
-            Color color = c.image.pixel_data[c.w * y + x];
-            printf(" #%06" PRIx32,
-                   (uint32_t)(color.r << 16 | color.g << 8 | color.b));
+static void canvas_draw_rectangle(Canvas *canvas, int x, int y, int w, int h, Color color) {
+    for (int nx = 0; nx < w; nx++) {
+        for (int ny = 0; ny < h; ny++) {
+            canvas_set_pixel(canvas, x + nx, y + ny, color);
         }
     }
-    printf("\n");
+}
+
+static void debug_canvas(Canvas *c) {
+    printf("Canvas:\n");
+    printf("  w: %d\n", c->w);
+    printf("  h: %d\n", c->h);
+    printf("  BMPImage:\n");
+    printf("    w: %d\n", c->image.w);
+    printf("    h: %d\n", c->image.h);
+
+    int hw = c->w / 2;
+    int hh = c->w / 2;
+
+    for (int i = -hw; i < hw; i++) {
+        canvas_set_pixel(c, i, 0, color_rgb(0x00, 0xff, 0x00));
+        canvas_set_pixel(c, 0, i, color_rgb(0xff, 0x00, 0xff));
+    }
+
+    canvas_draw_rectangle(c, 0, 0, 100, 100, color_rgb(0xff, 0xff, 0xff));
+    canvas_export(c, "canvas_test.bmp");
 }
 
 int main() {
-    Canvas c = canvas_new(10, 10);
-    canvas_set_pixel(&c, 0, 5, color_rgb(0xff, 0xff, 0xff));
-    debug_canvas(c);
+    Canvas c = canvas_new(600, 600);
 
+    debug_canvas(&c);
     canvas_free(&c);
     return 0;
 }
