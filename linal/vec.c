aa82b96fda6b12d1b37c10d4f4272359e4d4958c
brkp <brkp@tutanota.com>
2022-03-16 22:47:54 +0300

    _cross and _dot implementations

--- linal/vec.c
+++ linal/vec.c
@@ -19,3 +19,14 @@ Vec3 vec3_sub(Vec3 a, Vec3 b) {
     Vec3 v = { a.x - b.x, a.y - b.y, a.z - b.z };
     return v;
 }
+
+Vec3 vec3_cross(Vec3 a, Vec3 b) {
+    Vec3 v = { a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z,
+               a.x * b.y - a.y * b.x };
+    return v;
+}
+
+float vec3_dot(Vec3 a, Vec3 b) {
+    float d = a.x * b.x + a.y * b.y + a.z * b.z;
+    return d;
+}
