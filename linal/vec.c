2140f782e91e5eb5d34e6f97d543e0324a567b52
teknosa <burzum@mal.com>
2022-03-16 22:33:47 +0300

    'linal/vec.c' güncelle

--- linal/vec.c
+++ linal/vec.c
@@ -10,9 +10,6 @@ Vec3 vec3_new(float x, float y, float z) {
 }
 
 
-
-
-
 Vec3 vec3_mul(Vec3 a, float k) {
     Vec3 v = { a.x * k, a.y * k, a.z * k };
 
@@ -20,16 +17,12 @@ Vec3 vec3_mul(Vec3 a, float k) {
 
 }
 
-
-
 Vec3 vec3_add(Vec3 a, Vec3 b) {
     Vec3 v = { a.x + b.x, a.y + b.y, a.z + b.z };
 
     return v;
 
 }
-
-
 
 Vec3 vec3_sub(Vec3 a, Vec3 b) {
     Vec3 resultv = { a.x - b.x, a.y - b.y, a.z - b.z };
