d4f036ae7030e4a89584e207c17c7eabbf71a6c8
brkp <brkp@tutanota.com>
2022-03-16 14:49:36 +0300

    Vec3 method prototypes

---
+++ linal/vec.h
@@ -0,0 +1,17 @@
+struct Vec3 {
+    float x;
+    float y;
+    float z;
+};
+
+Vec3 vec3d_new(float x, float y, float z);
+
+Vec3 vec3d_mul(Vec3 a, float k);
+Vec3 vec3d_add(Vec3 a, Vec3 b);
+Vec3 vec3d_sub(Vec3 a, Vec3 b);
+
+Vec3 vec3d_norm(Vec3 vec);
+float vec3d_len(Vec3 vec);
+
+Vec3 vec3d_cross(Vec3 a, Vec3 b);
+float vec3d_dot(Vec3 a, Vec3 b);
