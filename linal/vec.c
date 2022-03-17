12f8138dceb4ad56bb1fa1f9349d8bbbd9b296de
Ny <ny@ny.local>
2022-03-17 20:53:52 +1100

    Made linal/vec.c functions one liners and added the clang-format accordingly

--- linal/vec.c
+++ linal/vec.c
@@ -1,32 +1,26 @@
 #include "vec.h"
 
 Vec3 vec3_new(float x, float y, float z) {
-    Vec3 v = { x, y, z };
-    return v;
+    return (Vec3){ x, y, z };
 }
 
 Vec3 vec3_mul(Vec3 a, float k) {
-    Vec3 v = { a.x * k, a.y * k, a.z * k };
-    return v;
+    return (Vec3){ a.x * k, a.y * k, a.z * k };
 }
 
 Vec3 vec3_add(Vec3 a, Vec3 b) {
-    Vec3 v = { a.x + b.x, a.y + b.y, a.z + b.z };
-    return v;
+    return (Vec3){ a.x + b.x, a.y + b.y, a.z + b.z };
 }
 
 Vec3 vec3_sub(Vec3 a, Vec3 b) {
-    Vec3 v = { a.x - b.x, a.y - b.y, a.z - b.z };
-    return v;
+    return (Vec3){ a.x - b.x, a.y - b.y, a.z - b.z };
 }
 
 Vec3 vec3_cross(Vec3 a, Vec3 b) {
-    Vec3 v = { a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z,
-               a.x * b.y - a.y * b.x };
-    return v;
+    return (Vec3){ a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z,
+                   a.x * b.y - a.y * b.x };
 }
 
 float vec3_dot(Vec3 a, Vec3 b) {
-    float d = a.x * b.x + a.y * b.y + a.z * b.z;
-    return d;
+    return (float)(a.x * b.x + a.y * b.y + a.z * b.z);
 }
