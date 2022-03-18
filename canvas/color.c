7bbb302f9096c4dd2aa1beedeb1d44f3dca70843
brkp <brkp@tutanota.com>
2022-03-18 23:55:30 +0300

    safe_add and safe_sub functions

--- canvas/color.c
+++ canvas/color.c
@@ -1,100 +1,37 @@
 #include "color.h"
 
+static inline uint8_t safe_add(uint8_t a, uint8_t b) {
+    if (UINT8_MAX - a < b)
+        return 0xff;
+
+    return a + b;
+}
+
+static inline uint8_t safe_sub(uint8_t a, uint8_t b) {
+    if (a < b)
+        return 0;
+
+    return a - b;
+}
+
 Color color_rgb(uint8_t r, uint8_t g, uint8_t b) {
     return (Color){ r, g, b };
 }
 
 Color color_add(Color a, Color b) {
-    Color tmp = { 0, 0, 0 };
-
-    if (UINT8_MAX - a.r < b.r) {
-        tmp.r = 0xff;
-    } else {
-        tmp.r = a.r + b.r;
-    }
-
-    if (UINT8_MAX - a.g < b.g) {
-        tmp.g = 0xff;
-    } else {
-        tmp.g = a.g + b.g;
-    }
-
-    if (UINT8_MAX - a.b < b.b) {
-        tmp.b = 0xff;
-    } else {
-        tmp.b = a.b + b.b;
-    }
-
-    return tmp;
+    return (Color){ safe_add(a.r, b.r), safe_add(a.g, b.g),
+                    safe_add(a.b, b.b) };
 }
 
 Color color_addk(Color a, uint8_t k) {
-    Color tmp = { 0, 0, 0 };
-
-    if (UINT8_MAX - a.r < k) {
-        tmp.r = 0xff;
-    } else {
-        tmp.r = a.r + k;
-    }
-
-    if (UINT8_MAX - a.g < k) {
-        tmp.g = 0xff;
-    } else {
-        tmp.g = a.g + k;
-    }
-
-    if (UINT8_MAX - a.b < k) {
-        tmp.b = 0xff;
-    } else {
-        tmp.b = a.b + k;
-    }
-
-    return tmp;
+    return (Color){ safe_add(a.r, k), safe_add(a.g, k), safe_add(a.b, k) };
 }
 
 Color color_sub(Color a, Color b) {
-    Color tmp = { 0, 0, 0 };
-    if (a.r < b.r) {
-        tmp.r = 0x00;
-    } else {
-        tmp.r = a.r - b.r;
-    }
-
-    if (a.g < b.b) {
-        tmp.g = 0x00;
-    } else {
-        tmp.g = a.g - b.g;
-    }
-
-    if (a.b < b.b) {
-        tmp.b = 0x00;
-    } else {
-        tmp.b = a.b - b.b;
-    }
-
-    return tmp;
+    return (Color){ safe_sub(a.r, b.r), safe_sub(a.g, b.g),
+                    safe_sub(a.b, b.b) };
 }
 
 Color color_subk(Color a, uint8_t k) {
-    Color tmp = { 0, 0, 0 };
-
-    if (a.r < k) {
-        tmp.r = 0x00;
-    } else {
-        tmp.r = a.r - k;
-    }
-
-    if (a.g < k) {
-        tmp.g = 0x00;
-    } else {
-        tmp.g = a.g - k;
-    }
-
-    if (a.b < k) {
-        tmp.b = 0x00;
-    } else {
-        tmp.b = a.b - k;
-    }
-
-    return tmp;
+    return (Color){ safe_sub(a.r, k), safe_sub(a.g, k), safe_sub(a.b, k) };
 }
