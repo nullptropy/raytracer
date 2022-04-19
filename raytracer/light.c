4d734c34050cb24eda23b97d2691804ced4172c2
brkp <brkp@tutanota.com>
2022-04-19 10:17:41 +0300

    intiial light type implementation

---
+++ raytracer/light.c
@@ -0,0 +1,14 @@
+#include "light.h"
+#include "linal/vec.h"
+
+Light light_new_point(float intensity, Vec3 position) {
+    return (Light){ Point, intensity, position, vec3_new(0, 0, 0) };
+}
+
+Light light_new_ambient(float intensity) {
+    return (Light){ Ambient, intensity, vec3_new(0, 0, 0), vec3_new(0, 0, 0) };
+}
+
+Light light_new_directional(float intensity, Vec3 direction) {
+    return (Light){ Directional, intensity, vec3_new(0, 0, 0), direction };
+}
---
+++ raytracer/light.h
@@ -0,0 +1,25 @@
+#include "linal/vec.h"
+
+typedef enum light_type {
+    Point,
+    Ambient,
+    Directional,
+} LightType;
+
+typedef struct light {
+    LightType type;
+    float intensity;
+
+    Vec3 position;  // this field is only valid for point lights
+    Vec3 direction; // this field is only valid for directional lights
+} Light;
+
+typedef struct light_array {
+    Light *values;
+    int num;
+    int cap;
+} LightArray;
+
+Light light_new_point(float intensity, Vec3 position);
+Light light_new_ambient(float intensity);
+Light light_new_directional(float intensity, Vec3 direction);
