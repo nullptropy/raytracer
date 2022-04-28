c53c071b2143c8ca90c1cde0e77ed57ceac649b8
brkp <brkp@tutanota.com>
2022-04-28 14:21:59 +0300

    e

--- raytracer/main.c
+++ raytracer/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "canvas/canvas.h"
 #include "linal/vec.h"
 
@@ -11,26 +13,16 @@ int main(void) {
     Scene scene = scene_new(camera_new(0, 0, 0), viewport_new(1, 1, 1),
                             color_rgb(0x000000));
 
-    /* scene_add_light(&scene, light_new_ambient(0.2)); */
-    /* scene_add_light(&scene, light_new_point(0.8, vec3_new(1, 1, 1))); */
-
-    /* scene_add_object( */
-    /*     &scene, sphere_new(1, vec3_new(-2, 3, 6), color_rgb(0x00ff00), 500)); */
-    /* scene_add_object( */
-    /*     &scene, sphere_new(1, vec3_new(0, 0, 6), color_rgb(0x58cced), 500)); */
-    /* scene_add_object( */
-    /*     &scene, sphere_new(1, vec3_new(2, 0, 4), color_rgb(0xff0000), 100)); */
-
     scene_add_light(&scene, light_new_ambient(0.2));
-    scene_add_light(&scene, light_new_point(0.6, vec3_new(2, 1, 0)));
+    scene_add_light(&scene, light_new_point(0.6, vec3_new(0, 1, 2)));
     scene_add_light(&scene, light_new_directional(0.2, vec3_new(1, 4, 4)));
 
     scene_add_object(
-        &scene, sphere_new(1, vec3_new(0x00, -0x1, 3), color_rgb(0xff0000), 500));
+        &scene, sphere_new(1, vec3_new(0, -1, 3), color_rgb(0xff0000), 500));
     scene_add_object(
-        &scene, sphere_new(1, vec3_new(-0x2, 0x00, 4), color_rgb(0x00ff00), 10));
+        &scene, sphere_new(1, vec3_new(-2, 0, 4), color_rgb(0x00ff00), 10));
     scene_add_object(
-        &scene, sphere_new(1, vec3_new(0x02, 0x00, 4), color_rgb(0x0000ff), 500));
+        &scene, sphere_new(1, vec3_new(2, 0, 4), color_rgb(0x0000ff), 500));
 
     scene_render(&scene, &canvas);
     canvas_export(&canvas, "raytraced.bmp");
